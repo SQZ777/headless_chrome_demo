4b14fdecaa9dd7d08aabdc3000bcca99981aabc2
sqz777 <sqz777isme@gmail.com>
2018-04-16 23:49:21 +0800

    final

--- features/my_first.feature
+++ features/my_first.feature
@@ -3,3 +3,8 @@ Feature: Go web site feature
   Scenario: As a valid user I can go to Git Hub
     Given I Go to git hub
     Then Take a picture
+  
+  @twitch
+  Scenario: As a valid user I can go to Twitch
+    Given I Go to Twitch
+    Then Take a picture
--- features/step_definitions/custom_steps.rb
+++ features/step_definitions/custom_steps.rb
@@ -3,7 +3,12 @@ require 'selenium-cucumber'
 Given("I Go to git hub") do
     navigate_to('https://github.com/')
 end
-  
+
+Given("I Go to Twitch") do
+    navigate_to('https://www.twitch.tv/')
+    wait(5)
+end
+
 Then("Take a picture") do
     take_screenshot
 end
